4a1f66b6d87069e42e756293e95f543603f939b0
Arne Wischer <Fettlaus@gmail.com>
2011-06-02 16:32:13 +0000

    Added ProcessImpl and LogImpl

---
+++ Client/src/client/LogImpl.java
@@ -0,0 +1,13 @@
+package client;
+
+import ggTCalculator.LogPOA;
+
+public class LogImpl extends LogPOA {
+
+    @Override
+    public void log(String user, String msg) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ Starter/src/starter/ProcessImpl.java
@@ -0,0 +1,123 @@
+package starter;
+
+import ggTCalculator.Log;
+import ggTCalculator.Process;
+import ggTCalculator.ProcessPOA;
+
+public class ProcessImpl extends ProcessPOA {
+
+    @Override
+    public Process left() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void left(Process newLeft) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Process right() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void right(Process newRight) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public int id() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void id(int newId) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public int number() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void number(int newNumber) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Log log() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void log(Log newLog) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public double delay() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void delay(double newDelay) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public int timeout() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void timeout(int newTimeout) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean ready() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void ready(boolean newReady) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean terminate() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void message(int number) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
