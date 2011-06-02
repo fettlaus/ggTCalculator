c4b6ae5c0827c2255d567071a8d75fc6b370a255
Arne Wischer <Fettlaus@gmail.com>
2011-06-02 14:41:39 +0000

    Added Starter files

---
+++ Starter/src/starter/StarterImpl.java
@@ -0,0 +1,25 @@
+package starter;
+
+import ggTCalculator.StarterPOA;
+
+public class StarterImpl extends StarterPOA {
+
+    @Override
+    public void createProcess(int count) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void quitProcess() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void quit() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
