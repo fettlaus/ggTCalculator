ef7d560ee6859ab3decb99ecbcee9a8dc99b0f9b
Arne Wischer <Fettlaus@gmail.com>
2011-06-03 00:55:32 +0000

    Added functionality to Client & LogImpl

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -1,13 +1,103 @@
 package client;
 
-public class Client {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+import ggTCalculator.*;
+import ggTCalculator.CoordinatorPackage.alreadyRunning;
+import ggTCalculator.CoordinatorPackage.noStarter;
 
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+import org.omg.PortableServer.Servant;
+
+
+public class Client 
+{
+	private static Log log;
+	
+	public static void main(String[] args)
+	{
+			BufferedReader input = new BufferedReader(new InputStreamReader(
+					System.in));
+
+		try 
+		{
+			    /* Create Corba Connection */
+				Properties props = new Properties();
+				String coordname;
+				String help;
+				System.out.println("Coordinatorname eingeben");
+				coordname = input.readLine();
+				
+				System.out.println("Um Hilfe bei der Eingabe zu bekomme tippen sie Hilfe ein, sonst drücken sie Enter");
+				help = input.readLine();
+				if(help.equalsIgnoreCase("Hilfe")){
+					help();
+				}else{
+					
+				}
+				
+				
+				ORB orb = ORB.init(args, props);
+				POA rootPoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+			    rootPoa.the_POAManager().activate();
+				NamingContextExt nc = NamingContextExtHelper.narrow(
+				orb.resolve_initial_references("NameService"));
+				/* Default Coordinator Name */
+				org.omg.CORBA.Object obj = nc.resolve_str(coordname);
+				Coordinator coord = CoordinatorHelper.narrow (obj);
+				LogImpl logI = new LogImpl();
+				log = LogHelper.narrow(rootPoa.servant_to_reference((Servant) logI));
+				
+				if(args.length > 2){
+					String cName = args[0];
+					int minggT = Integer.parseInt(args[2]);
+					int maxggT = Integer.parseInt(args[3]);
+					int minDe = Integer.parseInt(args[4]);
+					int maxDe = Integer.parseInt(args[5]);
+					int to = Integer.parseInt(args[6]);
+					int ggt = Integer.parseInt(args[7]);
+				
+					if(args[1].equalsIgnoreCase("calc")){
+						coord.calculate(to, minDe, maxDe, minggT, maxggT, ggt, log);
+					}else if(args[1].equalsIgnoreCase("quit")){
+						log.log(coordname, "beendet den Prozess");
+						coord.quit();
+						
+					}else if(args[1].equalsIgnoreCase("show")){
+						String[] s = coord.getStarterList();
+						log.log(coordname, "besorgt die Starterliste");
+						for(int i = 0; i < s.length; i++)
+							System.out.println(s[i]);
+					}else{
+						System.out.println("unbekannter Fehler");
+						log.log(coordname, "unbekannter Fehler");
+					}
+					
+				
+				
+				}
+				
+				
+		} 
+		catch (Exception ex) 
+		{
+				System.err.println (ex);
+				System.exit(1);
+		}
+		
+	}
+			
+	public static void help(){
+		
+	}
 
 }
--- Client/src/client/LogImpl.java
+++ Client/src/client/LogImpl.java
@@ -1,13 +1,40 @@
 package client;
 
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import ggTCalculator.LogPOA;
 
-public class LogImpl extends LogPOA {
+public class LogImpl extends LogPOA
+{
+	String file = "";
+	String row = "";
 
-    @Override
-    public void log(String user, String msg) {
-        // TODO Auto-generated method stub
 
-    }
-
+	@Override
+	public void log(String user, String msg) {
+		// TODO Auto-generated method stub
+		/* Print Log Entry and add to File List */
+		System.out.println(user + " : " + msg);
+		file += user + " : " + msg + "\n";
+		
+		/* Create Log File */
+		try
+		{
+			File file = new File(new SimpleDateFormat("HHmmss").format(new Date()) +".log");
+			FileWriter fw = new FileWriter(file);
+			fw.write(this.file);
+			fw.flush();
+			fw.close();
+		}
+		catch( IOException e )
+		{
+			e.printStackTrace();
+		}
+		
+	}
 }
